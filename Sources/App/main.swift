cdc613fd3d828c5a47c2dc3e6e2221b4dfd9faf1
roudique <roudique@gmail.com>
2016-11-13 20:32:34 -0500

    Add sending email

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,9 +1,36 @@
 import Vapor
 import Foundation
+import SMTP
+import Transport
 
 let drop = Droplet()
 let parser = FAPLParser()
 
+//MARK: Email
+
+func sendEmail() -> () {
+    let credentials = SMTPCredentials(user: "roudique", pass: "Jqegervsrw18")
+    let from = "roudique@gmail.com"
+    let to = "panzerfauster.wot@gmail.com"
+    
+    
+    
+    let email = Email(
+        from: from,
+        to: to,
+        subject: "Vapor SMTP - Simple",
+        body: "Hello from Vapor SMTP 👋"
+    )
+    
+    // MARK: Send
+    
+    let client = try! SMTPClient<TCPClientStream>.makeSendGridClient()
+    let (code, reply) = try! client.send(email, using: credentials)
+    print("Successfully sent email: \(code) \(reply)")
+    
+}
+
+//Mark: GET methods
 
 drop.get("post", ":number") { request in
     if let ID = request.parameters["number"]?.int {
@@ -19,8 +46,16 @@ drop.get("post", ":number") { request in
     let responseDict = ["status" : Node.init("error"),
                         "error" : Node.init("Post not found")]
     
+    sendEmail()
+    
     return try! JSON(node: Node.init(responseDict))
 }
 
+
+
+
+
+
 drop.run()
+
 
